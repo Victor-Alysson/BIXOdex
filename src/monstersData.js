81892a4d27dd70aa8d54d3fdeea590f878eb42ff
Victor_Alysson <victoralyssonrs@gmail.com>
2026-05-24 19:10:29 -0300

    feat(monstros): adiciona bixos

--- src/monstersData.js
+++ src/monstersData.js
@@ -12,14 +12,20 @@ import m9img from './assets/SONOnic.jpg';
 import m10img from './assets/wr.jpg';
 import m11img from './assets/pet-pembas.jpg';
 import m12img from './assets/emp.jpg';
+import m13img from './assets/pajet-marius.jpg';
+import m14img from './assets/pajet-noia.jpg';
+import m15img from './assets/pajet-love.jpg';
+import m16img from './assets/trakinas-marius.jpg';
+import m17img from './assets/trakinas-soninho.jpg';
+import m18img from './assets/pajet-cabeludus.jpg';
 
 export const monstersData = [
   {
     id: 1,
     name: "MOSQUITUS CABELUDUS",
     type: "Inseto",
-    level: 10,
-    hp: 180,
+    level: 1,
+    hp: 45,
     atk: 95,
     def: 78,
     image: m1img
@@ -28,8 +34,8 @@ export const monstersData = [
     id: 2,
     name: "MOSQUITUS GIGANTUS",
     type: "Inseto",
-    level: 15,
-    hp: 200,
+    level: 3,
+    hp: 65,
     atk: 110,
     def: 80,
     image: m2img
@@ -38,8 +44,8 @@ export const monstersData = [
     id: 3,
     name: "MOSQUITUS ENFORCADUS",
     type: "Inseto",
-    level: 20,
-    hp: 220,
+    level: 5,
+    hp: 90,
     atk: 120,
     def: 85,
     image: m3img 
@@ -48,8 +54,8 @@ export const monstersData = [
     id: 4,
     name: "CHAD BREAD",
     type: "Pão",
-    level: 25,
-    hp: 250,
+    level: 10,
+    hp: 140,
     atk: 130,
     def: 90,
     image: m4img
@@ -58,8 +64,8 @@ export const monstersData = [
     id: 5,
     name: "SP BREAD",
     type: "Pão",
-    level: 30,
-    hp: 270,
+    level: 15,
+    hp: 210,
     atk: 140,
     def: 95,
     image: m5img
@@ -68,8 +74,8 @@ export const monstersData = [
     id: 6,
     name: "DOCTOR BREAD",
     type: "Pão",
-    level: 35,
-    hp: 290,
+    level: 20,
+    hp: 300,
     atk: 150,
     def: 100,
     image: m6img
@@ -78,8 +84,8 @@ export const monstersData = [
     id: 7,
     name: "WATHERMELON",
     type: "Planta",
-    level: 40,
-    hp: 310,
+    level: 25,
+    hp: 420,
     atk: 160,
     def: 105,
     image: m7img
@@ -88,8 +94,8 @@ export const monstersData = [
     id: 8,
     name: "VELVUS",
     type: "Militar",
-    level: 45,
-    hp: 330,
+    level: 30,
+    hp: 580,
     atk: 170,
     def: 110,
     image: m8img
@@ -98,8 +104,8 @@ export const monstersData = [
     id: 9,
     name: "SONONIC",
     type: "Mendigo",
-    level: 50,
-    hp: 350,
+    level: 35,
+    hp: 750,
     atk: 180,
     def: 115,
     image: m9img
@@ -108,8 +114,8 @@ export const monstersData = [
     id: 10,
     name: "WR",
     type: "Gênio",
-    level: 55,
-    hp: 370,
+    level: 40,
+    hp: 950,
     atk: 190,
     def: 120,
     image: m10img
@@ -118,8 +124,8 @@ export const monstersData = [
     id: 11,
     name: "PET PEMBAS",
     type: "Dragão",
-    level: 60,
-    hp: 390,
+    level: 45,
+    hp: 1200,
     atk: 200,
     def: 125,
     image: m11img
@@ -128,10 +134,70 @@ export const monstersData = [
     id: 12,
     name: "EMPACOTADUS",
     type: "Militar",
-    level: 65,
-    hp: 410,
+    level: 50,
+    hp: 1500,
     atk: 210,
     def: 130,
     image: m12img
-  }
+  },
+  {
+    id: 13,
+    name: "PAJET MARIUS",
+    type: "Emo",
+    level: 55,
+    hp: 1850,
+    atk: 220,
+    def: 135,
+    image: m13img
+  },
+  {
+    id: 14,
+    name: "PAJET NOIA",
+    type: "Suspeito",
+    level: 60,
+    hp: 2250,
+    atk: 230,
+    def: 140,
+    image: m14img
+  },
+  {
+    id: 15,
+    name: "PAJET LOVE",
+    type: "E-Girl",
+    level: 65,
+    hp: 2700,
+    atk: 240,
+    def: 145,
+    image: m15img
+  },
+  {
+    id: 16,
+    name: "TRAKINAS MARIUS",
+    type: "Humano",
+    level: 70,
+    hp: 3200,
+    atk: 250,
+    def: 150,
+    image: m16img
+  },
+  {
+    id: 17,
+    name: "TRAKINAS SONINHO",
+    type: "Preguiça",
+    level: 75,
+    hp: 3800,
+    atk: 260,
+    def: 155,
+    image: m17img
+  },
+  {
+    id: 18,
+    name: "PAJET CABELUDUS",
+    type: "Preguiça",
+    level: 80,
+    hp: 4500,
+    atk: 270,
+    def: 160,
+    image: m18img
+}
 ];
